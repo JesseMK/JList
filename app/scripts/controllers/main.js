9a04881b8af76f9cff235c01b03eda3c7ba5a6bf
JesseMK <Jessewangzh@hotmail.com>
2016-05-29 15:45:28 +0800

    Demo done

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,23 @@
 'use strict';
 
 angular.module('jlistApp')
-  .controller('MainCtrl', function($scope) {
-    $scope.todos = [];
-    // $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  .controller('MainCtrl', function($scope, localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function() {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
-      $scope.todo = ''
+      $scope.todo = '';
     };
+
     $scope.removeTodo = function(index) {
       $scope.todos.splice(index, 1);
     };
+
   });
